856ce3a7f46df382f559efb3982739c12e7b8088
Daniel MC <daniel.meneses27@gmail.com>
2018-07-13 21:14:43 -0500

    add drone tests and remove old test file

--- src/test/scala/co/com/s4n/deliveries/DroneTest.scala
+++ src/test/scala/co/com/s4n/deliveries/DroneTest.scala
@@ -1,44 +1,12 @@
-import java.io.{BufferedWriter, File, FileWriter}
-
+package co.com.s4n.deliveries
 import co.com.s4n.deliveries.domain.entities.MapLimits
 import co.com.s4n.deliveries.domain.services.{DeliveryService, DroneService}
 import co.com.s4n.deliveries.infrastructure.FileAccess
 import org.scalatest._
 
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
-class test extends FunSuite {
-
-  test("can read a file") {
-    val file = Try(FileAccess.read("in.txt"))
-    assert(file.isSuccess)
-  }
-
-  test("if a bad file name is given should fail") {
-    val file = Try(FileAccess.read("badFile.txt"))
-    assert(file.isFailure)
-  }
-
-  test("can write a file") {
-    val write = Try(FileAccess.write("test.txt", "test message"))
-    assert(write.isSuccess)
-  }
-
-  test("writing a file should fail when i don't give a name or message") {
-    val write = Try(FileAccess.write("", "test message"))
-    val write2 = Try(FileAccess.write("text.txt", ""))
-    assert(write.isFailure)
-    assert(write2.isFailure)
-  }
-
-  test("can list files in a directory") {
-    val files = Try(FileAccess.list)
-    assert(files.isSuccess)
-    files.map(fileList => assert(0 < fileList.length))
-  }
-
-
-
+class DroneTest extends FunSuite {
   test("dron can make delivers") {
     val delivery = Try(List("ALR", "LRA"))
     val mapLimit = new MapLimits(10, 10, -10, -10)
@@ -75,5 +43,4 @@ class test extends FunSuite {
     assert(deliveriesResult.isSuccess)
 
   }
-
 }
