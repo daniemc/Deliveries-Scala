22fa33f6b6ea2b8bf04ee03a22b1065d05057c2d
daniemc <daniel.meneses27@gmail.com>
2018-07-16 11:54:46 -0500

    add drone tests

--- src/test/scala/co/com/s4n/deliveries/DroneTest.scala
+++ src/test/scala/co/com/s4n/deliveries/DroneTest.scala
@@ -1,5 +1,6 @@
 package co.com.s4n.deliveries
-import co.com.s4n.deliveries.domain.entities.MapLimits
+import co.com.s4n.deliveries.domain.VO.N
+import co.com.s4n.deliveries.domain.entities.{MapLimits, Position}
 import co.com.s4n.deliveries.domain.services.{DeliveryService, DroneService, MapLimitsService}
 import co.com.s4n.deliveries.infrastructure.FileAccess
 import org.scalatest._
@@ -7,12 +8,37 @@ import org.scalatest._
 import scala.util.Try
 
 class DroneTest extends FunSuite {
+
+  test("an input and output file name must") {
+    val fileName = DroneService.output("01")
+    assert("out01.txt" == fileName)
+  }
+
+  test("a dron can be prepared") {
+    val preparedDrone = DroneService.prepareDrone("01")
+    assert(preparedDrone.isSuccess)
+  }
+
+  test("a dron can deliver an order at a given position") {
+    val position = new Position(2, 4, N())
+    val droneName = "outTest.txt"
+    val deliver = DroneService.deliverOrder(position, droneName)
+    val file = FileAccess.read(droneName)
+    assert("Delivery: (2, 4 - N())" == file(0))
+  }
+
   test("dron can make delivers") {
     val delivery = Try(List("ALR", "LRA"))
     val deliveries = DeliveryService.prepareDelivery(delivery, 3)
     val drone = DroneService.prepareDrone("01")
     val deliveriesResult =  Try(DroneService.makeDeliveries(drone, deliveries, MapLimitsService.defaultMap))
     assert(deliveriesResult.isSuccess)
+  }
+
+  test("you can get the drone name of input file delivery") {
+    val droneName = "01"
+    val fileName = "in01.txt"
+    assert(droneName == DroneService.getDroneNameFromFile(fileName))
   }
 
   test("pass bad moves to drone will fail") {
