4b6a96abc85d5a0423c076c1768792d8877e98f4
Daniel MC <daniel.meneses27@gmail.com>
2018-07-15 23:15:30 -0500

    add method to execute several drones in a future with custom executor service

--- src/main/scala/co/com/s4n/deliveries/domain/services/DroneService.scala
+++ src/main/scala/co/com/s4n/deliveries/domain/services/DroneService.scala
@@ -1,9 +1,13 @@
 package co.com.s4n.deliveries.domain.services
 
+import java.util.concurrent.Executors
+import java.util.regex.Pattern
+
 import co.com.s4n.deliveries.domain.VO.{Moves, N}
 import co.com.s4n.deliveries.domain.entities._
 import co.com.s4n.deliveries.infrastructure.FileAccess
 
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
 object DroneService {
@@ -26,19 +30,16 @@ object DroneService {
   }
 
   def move(movement: Try[Moves], drone: Try[Drone], cityMap: MapLimits): Try[Drone] = {
-
     movement match {
       case Success(step) => {
-        val newPosition = PositionService.reposition(step, drone.map(_.position), cityMap)
-
+        val newPosition = PositionService.reposition(step, drone.map(_.position), cityMap, drone.get.output)
         newPosition match {
-          case Success(newPoss) => drone.map(dr => new Drone(dr.name, dr.input, dr.input, newPoss))
+          case Success(newPoss) => drone.map(dr => new Drone(dr.name, dr.input, dr.output, newPoss))
           case Failure(err) => {
             reportError(drone.get.name, err.getMessage)
             Failure(new Exception(err.getMessage))
           }
         }
-
       }
       case Failure(err) => {
         reportError(drone.get.name, err.getMessage)
@@ -52,10 +53,41 @@ object DroneService {
     delivery.route.map(action => deliveryDrone = DroneService.move(action, deliveryDrone, cityMap))
   }
 
-  def delivery(position: Position): Position = {
+  def delivery(position: Position, name: String): Position = {
     val message = s"Delivery: (${position.x}, ${position.y} - ${position.o})"
-    FileAccess.write("out.txt", message)
+    FileAccess.write(name, message)
     position
+  }
+
+  def multiDroneDelivery(deliveriesList: List[String]) = {
+    deliveriesList.map(delivery => {
+      val deliveryFile = Try(FileAccess.read(delivery))
+      val mapLimit = new MapLimits(10, 10, -10, -10)
+      val del = DeliveryService.prepareDelivery(deliveryFile, 10)
+      val name = getDroneNameFromFile(delivery)
+      val drone = prepareDrone(name)
+      val exCont = ExCont
+      Future {
+        DroneService.makeDeliveries(drone, del, mapLimit)
+      }{exCont}
+    })
+  }
+
+  def ExCont: ExecutionContext = {
+    implicit val exCont = new ExecutionContext {
+      val threadPool = Executors.newFixedThreadPool(20)
+      def execute(runnable: Runnable) {
+        threadPool.submit(runnable)
+      }
+      def reportFailure(t: Throwable) {}
+    }
+    exCont
+  }
+
+  def getDroneNameFromFile(file: String): String = {
+    val partName = file.split(Pattern.quote("."))
+    val name = partName(0).substring(2)
+    name
   }
 
   def reportError(droneName: String, message: String) = {
