2d57b4d7c81f79a83a98c050c307f482b5351a0d
daniemc <daniel.meneses27@gmail.com>
2018-07-16 16:00:58 -0500

    drone service refactor to make algebra and interpretation

--- src/main/scala/co/com/s4n/deliveries/domain/services/DroneService.scala
+++ src/main/scala/co/com/s4n/deliveries/domain/services/DroneService.scala
@@ -2,21 +2,34 @@ package co.com.s4n.deliveries.domain.services
 
 import java.util.regex.Pattern
 
-import co.com.s4n.deliveries.domain.VO.{Move, N}
+import co.com.s4n.deliveries.domain.VO.{Move}
 import co.com.s4n.deliveries.domain.entities._
 import co.com.s4n.deliveries.infrastructure.{DeliveriesExecutor, FileAccess}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{Future}
 import scala.util.{Failure, Success, Try}
 
-object DroneService {
-  def defaultDrone: Drone = new Drone("", "", PositionService.defaultPosition)
+sealed trait DroneAlgebra {
+  def defaultDrone: Drone
+  def output(name: String): String
+  def prepareDrone(name: String): Try[Drone]
+  def move(movement: Try[Move], drone: Try[Drone], cityMap: MapLimits): Try[Drone]
+  def goToNewPosition(step: Move, drone: Try[Drone], cityMap: MapLimits): Try[Drone]
+  def makeDeliveries(drone: Try[Drone], delivery: Delivery, cityMap: MapLimits): Try[Drone]
+  def deliverOrder(position: Position, name: String): Position
+  def multiDroneDelivery(deliveriesList: List[String]): List[Future[Try[Drone]]]
+  def getDroneNameFromFile(file: String): String
+  def reportError(droneName: String, message: String): Unit
+}
 
-  def output(name: String): String =s"out${name}.txt"
+sealed trait DroneIntepretation extends DroneAlgebra {
+  override def defaultDrone: Drone = new Drone("", "", PositionService.defaultPosition)
 
-  def prepareDrone(name: String) : Try[Drone] = Try(new Drone(name, output(name), PositionService.defaultPosition))
+  override def output(name: String): String =s"out${name}.txt"
 
-  def move(movement: Try[Move], drone: Try[Drone], cityMap: MapLimits): Try[Drone] = movement match {
+  override def prepareDrone(name: String) : Try[Drone] = Try(new Drone(name, output(name), PositionService.defaultPosition))
+
+  override def move(movement: Try[Move], drone: Try[Drone], cityMap: MapLimits): Try[Drone] = movement match {
     case Success(step) => goToNewPosition(step, drone, cityMap)
     case Failure(err) => {
       reportError(drone.get.name, err.getMessage)
@@ -24,7 +37,7 @@ object DroneService {
     }
   }
 
-  def goToNewPosition(step: Move, drone: Try[Drone], cityMap: MapLimits) = {
+  override def goToNewPosition(step: Move, drone: Try[Drone], cityMap: MapLimits) = {
     PositionService.reposition(
       step,
       drone.map(_.position),
@@ -38,19 +51,19 @@ object DroneService {
     }
   }
 
-  def makeDeliveries(drone: Try[Drone], delivery: Delivery, cityMap: MapLimits) = {
+  override def makeDeliveries(drone: Try[Drone], delivery: Delivery, cityMap: MapLimits) = {
     delivery.route.foldLeft(drone) {(deliveryDrone, action) => {
       DroneService.move(action, deliveryDrone, cityMap)
     }}
   }
 
-  def deliverOrder(position: Position, name: String): Position = {
+  override def deliverOrder(position: Position, name: String): Position = {
     val message = s"Delivery: (${position.x}, ${position.y} - ${position.o})"
     FileAccess.write(name, message)
     position
   }
 
-  def multiDroneDelivery(deliveriesList: List[String]) = {
+  override def multiDroneDelivery(deliveriesList: List[String]) = {
     deliveriesList.map { deliveryFileName =>
       val delivery = DeliveryService.getDelivery(deliveryFileName)
       val drone = prepareDrone(getDroneNameFromFile(deliveryFileName))
@@ -61,13 +74,15 @@ object DroneService {
     }
   }
 
-  def getDroneNameFromFile(file: String): String = {
+  override def getDroneNameFromFile(file: String): String = {
     val partName = file.split(Pattern.quote("."))
     val name = partName(0).substring(2)
     name
   }
 
-  def reportError(droneName: String, message: String) = {
+  override def reportError(droneName: String, message: String) = {
     FileAccess.write("ErrorsReport.txt", s"[Error] Drone ${droneName} says: ${message}")
   }
 }
+
+object DroneService extends DroneIntepretation
