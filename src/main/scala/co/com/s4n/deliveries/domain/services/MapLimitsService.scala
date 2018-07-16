a913c9d150f3473c1ca0727cfa4f17c29c2ee80c
daniemc <daniel.meneses27@gmail.com>
2018-07-16 16:08:04 -0500

    refactor map limits to make alegra and interpretation

--- src/main/scala/co/com/s4n/deliveries/domain/services/MapLimitsService.scala
+++ src/main/scala/co/com/s4n/deliveries/domain/services/MapLimitsService.scala
@@ -4,21 +4,29 @@ import co.com.s4n.deliveries.domain.entities.{MapLimits, Position}
 
 import scala.util.{Failure, Success, Try}
 
-object MapLimitsService {
-  def defaultMap: MapLimits = {
+sealed trait MapLimitsAlgebra {
+  def defaultMap: MapLimits
+  def validateNS(position: Position, cityMap: MapLimits): Try[Position]
+  def validateEO(position: Position, cityMap: MapLimits): Try[Position]
+  def mapException(position: Position): Try[Position]
+}
+
+sealed trait  MapLimitsInterpretation extends MapLimitsAlgebra {
+  override def defaultMap: MapLimits = {
     new MapLimits(10, 10, -10, -10)
   }
 
-  def validateNS(position: Position, cityMap: MapLimits): Try[Position] = {
+  override def validateNS(position: Position, cityMap: MapLimits): Try[Position] = {
     if (position.y >= cityMap.S && position.y <= cityMap.N) Success(position) else mapException(position)
   }
 
-  def validateEO(position: Position, cityMap: MapLimits): Try[Position] = {
+  override def validateEO(position: Position, cityMap: MapLimits): Try[Position] = {
     if (position.x >= cityMap.O && position.x <= cityMap.E) Success(position) else mapException(position)
   }
 
-  def mapException(position: Position): Try[Position] = {
+  override def mapException(position: Position): Try[Position] = {
     Failure(new Exception(s"Can't go beyond city map limits, Pos:($position)"))
   }
-
 }
+
+object MapLimitsService extends MapLimitsInterpretation
