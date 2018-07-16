8070c034bbb63c440583185e0f273d79826a9a86
daniemc <daniel.meneses27@gmail.com>
2018-07-16 10:33:06 -0500

    refactor the way to get a delivery

--- src/main/scala/co/com/s4n/deliveries/domain/services/DeliveryService.scala
+++ src/main/scala/co/com/s4n/deliveries/domain/services/DeliveryService.scala
@@ -2,10 +2,17 @@ package co.com.s4n.deliveries.domain.services
 
 import co.com.s4n.deliveries.domain.VO._
 import co.com.s4n.deliveries.domain.entities._
+import co.com.s4n.deliveries.infrastructure.FileAccess
 
 import scala.util.{Failure, Success, Try}
 
 object DeliveryService {
+
+  def getDelivery(deliveryFileName: String): Delivery ={
+    val deliveryFileContent = Try(FileAccess.read(deliveryFileName))
+    DeliveryService.prepareDelivery(deliveryFileContent, 10)
+  }
+
   def prepareDelivery(delivery: Try[List[String]], deliveriesNumber: Int) : Delivery = {
     delivery match {
       case Success(deliver) => new Delivery(deliver.take(deliveriesNumber)
