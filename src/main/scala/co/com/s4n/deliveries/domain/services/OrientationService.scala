6d34536db096a1816fb60189b3a637fc9052ccf7
daniemc <daniel.meneses27@gmail.com>
2018-07-16 16:08:21 -0500

    refactor orientation service to make alegra and interpretation

--- src/main/scala/co/com/s4n/deliveries/domain/services/OrientationService.scala
+++ src/main/scala/co/com/s4n/deliveries/domain/services/OrientationService.scala
@@ -3,18 +3,25 @@ package co.com.s4n.deliveries.domain.services
 import co.com.s4n.deliveries.domain.VO.{E, N, O, S}
 import co.com.s4n.deliveries.domain.entities.{Position}
 
-object OrientationService {
-  def lFrom(position: Position): Position = position.o match {
+sealed trait OrientationAlebra {
+  def lFrom(position: Position): Position
+  def rFrom(position: Position): Position
+}
+
+sealed trait OrientationInterpretation extends OrientationAlebra {
+  override def lFrom(position: Position): Position = position.o match {
     case N() => new Position(position.x, position.y, O())
     case E() => new Position(position.x, position.y, N())
     case S() => new Position(position.x, position.y, E())
     case O() => new Position(position.x, position.y, S())
   }
 
-  def rFrom(position: Position): Position = position.o match {
+  override def rFrom(position: Position): Position = position.o match {
     case N() => new Position(position.x, position.y, E())
     case O() => new Position(position.x, position.y, N())
     case S() => new Position(position.x, position.y, O())
     case E() => new Position(position.x, position.y, S())
   }
 }
+
+object OrientationService extends OrientationInterpretation
